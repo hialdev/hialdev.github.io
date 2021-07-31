a9280d2e3720112ca06e8114b99f34cf9c937cfa
hialdev <al.developer12@gmail.com>
2021-07-31 20:45:55 +0700

    New Hialdev

---
+++ src/js/style.js
@@ -0,0 +1,12 @@
+
+$('.toggle-theme .dark').click(function(){
+    $('body').addClass('dark-theme');
+});
+$('.toggle-theme .light').click(function(){
+    $('body').removeClass('dark-theme');
+});
+//------------------------------
+//Slider
+//-----------------------------
+
+//Learn
